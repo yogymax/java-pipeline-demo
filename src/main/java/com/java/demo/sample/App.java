9da39a043786d2b4e840497f2fb94ac9b1b1bab3
Yogesh C <yogymax@gmail>
2023-03-29 13:42:13 +0530

    first commit

---
+++ src/main/java/com/java/demo/sample/App.java
@@ -0,0 +1,13 @@
+package com.java.demo.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/java/demo/sample/JDBCDemo.java
@@ -0,0 +1,9 @@
+package com.java.demo.sample;
+
+public class JDBCDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("Main method started...!");
+	}
+
+}
